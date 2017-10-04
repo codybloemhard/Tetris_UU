376af9cae44b0b9f5d5ea205586325580dbbf549
Cody Bloemhard <cblmhrd@live.nl>
2017-10-04 15:50:40 +0200

    git test
    ok

---
+++ Tetris/States/ClassicTetris.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tetris.States
+{
+    public class ClassicTetris : GameState
+    {
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch, GraphicsDevice device)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
